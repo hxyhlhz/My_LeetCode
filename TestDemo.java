47f5f07a75fff8762e948e571169e6c270b73743
liuhz1995 <liuhz1995@163.com>
2021-02-03 20:49:09 +0800

    LeetCode刷题记录第一版

---
+++ TestDemo.java
@@ -0,0 +1,11 @@
+package LeetCode;
+
+
+public class TestDemo {
+    public static void main(String[] args) {
+        String str = "AABAB";
+        int k = 1;
+        que1 qu = new que1();
+        int num = qu.characterReplacement(str, k);
+    }
+}
